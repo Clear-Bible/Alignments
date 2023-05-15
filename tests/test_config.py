758cfa9ca8f1fddad4dd92be830530dd7a1700ac
Sean Boisen <sean@seanboisen.com>
2023-05-15 15:04:23 -0400

    Added TestVersionSource

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,3 +12,22 @@ class TestConfig:
         """Test values."""
         # not much to test here
         assert Path(__file__).parent.parent == config.ROOT
+
+
+class TestVersionSource:
+    """Test VersionSource class."""
+
+    def test_init(self) -> None:
+        """Test initialization."""
+        vs = config.VersionSource(version="ESV", sourceid="WLC", language="eng", processid="manual")
+        assert vs.version == "ESV"
+        assert vs.sourceid == config.SourceidEnum.WLC
+        assert vs.language == "eng"
+        assert vs.processid == "manual"
+        assert vs.sourcepath == config.SOURCES / "WLC-ESV.tsv"
+        assert vs.targetpath == config.TARGETS / "WLC-ESV.tsv"
+        adirpath = config.ALIGNMENTS / vs.language / vs.version
+        assert vs.alignmentsdirpath == adirpath
+        assert vs.alignmentspath == adirpath / "WLC-ESV-manual.json"
+        assert vs.configpath == adirpath / "WLC-ESV-manual.toml"
+        assert vs.namespath == config.NAMES / "WLC-ESV.tsv"
