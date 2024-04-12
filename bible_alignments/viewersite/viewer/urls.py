1a03a2510d9f7fc1634cd0f8b9555426e9636431
Sean Boisen <sean@seanboisen.com>
2024-04-12 16:03:41 -0700

    drop viewersite

--- bible_alignments/viewersite/viewer/urls.py
+++
@@ -1,7 +0,0 @@
-"""Define URLs for the viewer app."""
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [path("", views.index, name="index")]
--- bible_alignments/viewersite/viewer/views.py
+++
@@ -1,9 +0,0 @@
-"""Views for the viewer app."""
-
-# from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    """Render a hello world."""
-    return HttpResponse("Hello viewing world.")
