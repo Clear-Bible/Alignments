b532da9ff3bcc07b29bff91176a0b484ac229f6b
Sean Boisen <sean@seanboisen.com>
2024-07-17 16:58:10 -0700

    new, from internal

---
+++ tests/bible_alignments/burrito/test_AlignmentSet.py
@@ -0,0 +1,88 @@
+"""Test code in src.burrito.AlignmentSet."""
+
+import pytest
+
+from src import DATAPATH, SOURCES
+from src.burrito import AlignmentSet
+
+ENGLANGDATAPATH = DATAPATH.parent.parent / "alignments-eng/data"
+HINLANGDATAPATH = DATAPATH.parent.parent / "alignments-hin/data"
+
+
+@pytest.fixture
+def aset() -> AlignmentSet:
+    """Return a AlignmentSet instance."""
+    return AlignmentSet(
+        sourceid="SBLGNT", targetid="LEB", targetlanguage="eng", langdatapath=ENGLANGDATAPATH, alternateid="manual"
+    )
+
+
+@pytest.fixture
+def asethin() -> AlignmentSet:
+    """Return a AlignmentSet instance for IRVHin."""
+    return AlignmentSet(
+        sourceid="SBLGNT", targetid="IRVHin", targetlanguage="hin", langdatapath=HINLANGDATAPATH, alternateid="manual"
+    )
+
+
+class TestAlignmentSet:
+    """Test the AlignmentSet class."""
+
+    def test_init(self, aset: AlignmentSet) -> None:
+        """Test the constructor."""
+        assert aset.identifier == "SBLGNT-LEB-manual"
+        assert aset.sourcedatapath == SOURCES
+        assert aset.canon == "nt"
+
+    def test_canon(self) -> None:
+        """Test the canon property."""
+        wlc = AlignmentSet(
+            sourceid="WLC", targetid="LEB", targetlanguage="eng", langdatapath=ENGLANGDATAPATH, alternateid="manual"
+        )
+        assert wlc.canon == "ot"
+        # should also test X value for not nt or ot
+
+    def test_sourceid(self) -> None:
+        """Test the constructor with various id values."""
+        with pytest.raises(ValueError):
+            AlignmentSet(sourceid="SBLGNT!", targetid="LEB", targetlanguage="eng")
+        with pytest.raises(ValueError):
+            AlignmentSet(sourceid="SBL-GNT", targetid="LEB", targetlanguage="eng")
+        with pytest.raises(ValueError):
+            AlignmentSet(sourceid="SBL.GNT", targetid="LEB", targetlanguage="eng")
+
+    def test_noalternateid(self) -> None:
+        """Test the constructor without alternateid value."""
+        cat = AlignmentSet(sourceid="SBLGNT", targetid="LEB", targetlanguage="eng")
+        assert cat.identifier == "SBLGNT-LEB"
+
+    def test_paths(self, aset: AlignmentSet) -> None:
+        """Test the source, target, alignment, and TOML paths."""
+        assert aset.sourcepath == SOURCES / "SBLGNT.tsv"
+        assert aset.targetpath == ENGLANGDATAPATH / "targets/LEB/nt_LEB.tsv"
+        # assert aset.langtargetdirpath == DATAPATH / "alignments/eng/LEB"
+        assert aset.alignmentpath == ENGLANGDATAPATH / "alignments/LEB/SBLGNT-LEB-manual.json"
+        assert aset.tomlpath == ENGLANGDATAPATH / "alignments/LEB/SBLGNT-LEB-manual.toml"
+
+
+class TestAlignmentSetHin:
+    """Test the AlignmentSet class for Hindi."""
+
+    def test_init(self, asethin: AlignmentSet) -> None:
+        """Test the constructor."""
+        assert asethin.identifier == "SBLGNT-IRVHin-manual"
+        assert asethin.sourcedatapath == SOURCES
+        assert asethin.canon == "nt"
+
+    def test_canon(self, asethin: AlignmentSet) -> None:
+        """Test the canon property."""
+        assert asethin.canon == "nt"
+        # should also test X value for not nt or ot
+
+    def test_paths(self, asethin: AlignmentSet) -> None:
+        """Test the source, target, alignment, and TOML paths."""
+        assert asethin.sourcepath == SOURCES / "SBLGNT.tsv"
+        assert asethin.targetpath == HINLANGDATAPATH / "targets/IRVHin/nt_IRVHin.tsv"
+        # assert asethin.langtargetdirpath == DATAPATH / "alignments/IRVHin"
+        assert asethin.alignmentpath == HINLANGDATAPATH / "alignments/IRVHin/SBLGNT-IRVHin-manual.json"
+        assert asethin.tomlpath == HINLANGDATAPATH / "alignments/IRVHin/SBLGNT-IRVHin-manual.toml"
---
+++ tests/bible_alignments/burrito/test_BaseToken.py
@@ -0,0 +1,38 @@
+"""Test code in src.burrito.BaseToken
+
+Sources and targets are tested more thoroughly in their respective
+test files: this only tests common functionality.
+
+"""
+
+import pytest
+
+from src.burrito import BaseToken, asbool
+
+
+@pytest.fixture
+def mrk_4_9_4() -> BaseToken:
+    """Return a BaseToken instance."""
+    return BaseToken(id="410040090051", text="ὦτα", altId="ὦτα-1")
+
+
+class TestBaseToken:
+    """Test BaseToken()."""
+
+    def test_init(self, mrk_4_9_4: BaseToken) -> None:
+        """Test initialization."""
+        assert mrk_4_9_4.id == "410040090051"
+        assert mrk_4_9_4.text == "ὦτα"
+        assert mrk_4_9_4.altId[:-2] == mrk_4_9_4.text
+        # default values
+        assert not mrk_4_9_4.aligned
+        assert mrk_4_9_4.text_unique == ""
+
+
+class TestAsbool:
+    """Test asbool()."""
+
+    def test_values(self) -> None:
+        """Test values."""
+        assert asbool(True) == "y"
+        assert asbool(False) == "n"
